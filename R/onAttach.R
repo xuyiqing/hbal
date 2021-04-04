a5c2ebd04eacc64a690dee063aa10f7e5ee06037
Eddie Yang <43977631+EddieYang211@users.noreply.github.com>
2021-04-03 17:41:44 -0700

    1.1.1

---
+++ R/onAttach.R
@@ -0,0 +1,15 @@
+#' @title Subsidiary hbal Function
+#' @description Function to load package description.
+#' @param lib   libname
+#' @param pkg   package name
+#' @importFrom utils packageDescription
+#' @references Xu, Y., & Yang, E. (2021). Hierarchically Regularized Entropy Balancing.
+
+.onAttach <- function(lib, pkg) {
+  mylib <- dirname(system.file(package = pkg))
+  title <- packageDescription(pkg, lib.loc = mylib)$Title
+  ver <- packageDescription(pkg, lib.loc = mylib)$Version
+  #author <- packageDescription(pkg, lib.loc = mylib)$Author
+  packageStartupMessage(pkg, ": ", title, "\nVersion: ", ver, "\nReference: ", 
+  	"Xu, Y., & Yang, E. (2021). Hierarchically Regularized Entropy Balancing.", "\n")
+}
